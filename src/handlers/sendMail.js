0e29bda011299af8611a8b63b1738c5c3c3e8da1
anthonyreis <matheus.anthony1@gmail.com>
2022-01-27 08:16:53 -0300

    adicionando função de envio de email

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,8 +1,35 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'sa-east-1' });
+
 async function sendMail(event, context) {
-  console.log(event);
-  
-  return event;
-}
+  const params = {
+    Source: 'emailjogos.max@gmail.com',
+    Destination: {
+      toAddresses: ['matheus.anthony1@gmail.com'],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from my notification service!',
+        },
+      },
+      Subject: {
+        Data: 'Test Mail',
+      },
+    }
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+
+    console.log(result);
+
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 export const handler = sendMail;
 
